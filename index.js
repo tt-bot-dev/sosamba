a6b53b2b766b29ba538764b33f87fe8d3df30c49
TTtie <tttiett@outlook.com>
2019-02-15 21:09:39 +0100

    The first working prototype of Sosamba is here

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const Client = require("./lib/Client");
-const Event = require("./lib/Event");
+const Event = require("./lib/structures/Event");
 const Logger = require("./lib/util/Logger");
+const Command = require("./lib/structures/Command");
+const ArgumentParser = require("./lib/structures/ArgumentParser");
+const SimpleArgumentParser = require("./lib/argParsers/SimpleArgumentParser");
 module.exports = {
     Client,
     Event,
     Logger,
+    Command,
+    ArgumentParser
 }
