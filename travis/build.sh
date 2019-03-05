0622a3b63426e5ad61c215ef13ebdf7dbcf9088f
TTtie <tttiett@outlook.com>
2019-03-05 20:24:27 +0100

    Try signing ourselves so it builds and commits

--- travis/build.sh
+++ travis/build.sh
@@ -6,8 +6,16 @@ if [ "$(git log -1 $TRAVIS_COMMIT --pretty="%aN")" == "tt-bot" ]; then
     exit 0
 fi
 
+echo "Generating documentation..."
 node generateDocs
 
+git config --global user.name "tt.bot" >/dev/null 2>&1
+# this is on purpose, it's public, feel free to email me! - TTtie 2019
+git config --global user.email \
+    "whatdidyouthinkiwontleakmyprivate@email.cz" \
+    >/dev/null 2>&1 
+
+echo "Adding the documentation to git update list"
 git add docs/
 
 git commit -m "Build documentation for $TRAVIS_COMMIT"
