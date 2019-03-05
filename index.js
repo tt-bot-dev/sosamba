9e18a9b14f9b8b9ebee472538887900985aeac00
TTtie <tttiett@outlook.com>
2019-03-05 08:13:38 +0100

    Progress on switch parsing Quotes on the argument parser now must start and end with the same quote; (else it will continuously parse everything as one switch) The switch parser is now added in (currently parsing just JS primitives) ParsingErrors, that are ignored when thrown in an event/arg parser New event: sosambaCommandError

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const Logger = require("./lib/util/Logger");
 const Command = require("./lib/structures/Command");
 const ArgumentParser = require("./lib/structures/ArgumentParser");
 const SimpleArgumentParser = require("./lib/argParsers/SimpleArgumentParser");
+const SwitchArgumentParser = require("./lib/argParsers/SwitchArgumentParser");
+const ParsingError = require("./lib/argParsers/ParsingError");
 module.exports = {
     Client,
     Event,
     Logger,
     Command,
-    ArgumentParser
+    ArgumentParser,
+    SimpleArgumentParser,
+    SwitchArgumentParser,
+    ParsingError
 }
