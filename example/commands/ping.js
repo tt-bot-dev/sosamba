de36f5e89a5c3d655c85dae32f877512bf8251e2
TTtie <tttiett@outlook.com>
2019-05-28 16:07:14 +0200

    Enforce strict mode

--- example/commands/ping.js
+++ example/commands/ping.js
@@ -1,3 +1,4 @@
+"use strict";
 const { Command } = require("../../lib/sosamba");
 class PingCommand extends Command {
     constructor(...args) {
--- example/index.js
+++ example/index.js
@@ -1,3 +1,4 @@
+"use strict";
 const {Client} = require("../index");
 const { createWriteStream } = require("fs");
 const bot = new Client("TOKEN", {
