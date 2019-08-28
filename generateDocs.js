56f7d4e4a8595e0d1cf89f99c7f2ae43129c583f
TTtie <tttiett@outlook.com>
2019-08-28 09:57:24 +0200

    refactor doc gen, use abal's erlpack

--- generateDocs.js
+++ generateDocs.js
@@ -1,9 +1,9 @@
 "use strict";
+/* eslint-disable no-console */
 let Docma;
 try {
     Docma = require("docma");
 } catch {
-    // eslint-disable-next-line no-console
     console.error("Please install Docma.");
     process.exit(1);
 }
@@ -12,7 +12,6 @@ Docma.create()
     .build(require(`${__dirname}/docma.json`))
     .then(() => {
         const [secs, nsecs] = process.hrtime(hrtime);
-        // eslint-disable-next-line no-console
         console.log(`Documentation built in ${secs}s ${nsecs / 1e6}ms`);
     })
-    .catch(console.error); // eslint-disable-line no-console
+    .catch(console.error);
