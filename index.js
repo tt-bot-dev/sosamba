a54ed9a183ace81613cfd370ea6fcc6db43f2597
TTtie <tttiett@outlook.com>
2019-12-03 19:48:36 +0100

    Put in all the new goodies IMPORTANT: This commit removes the documentation for the time being. Please refer to the code instead.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const ParsingError = require("./lib/argParsers/ParsingError");
 const Constants = require("./lib/Constants");
 const ReactionMenu = require("./lib/structures/ReactionMenu");
 const GlobalUser = require("./lib/structures/serializer/GlobalUser");
+const Integer = require("./lib/structures/serializer/Integer")
 const { constructQuery } = require("./lib/argParsers/switchSerializers/erisObjects");
 const Structures = require("./lib/Structures");
 const MessageListener = require("./lib/structures/MessageListener");
@@ -20,7 +21,6 @@ const {Member, User, Guild, GuildChannel} = Eris;
 /**
  * An object containing all the serializable classes.
  * Some symbols are re-exported from Eris.
- * @memberof Sosamba
  */
 const Serializers = {
     GlobalUser,
@@ -42,12 +42,12 @@ const Serializers = {
     /**
      * Re-exported Eris symbol
      */
-    GuildChannel
+    GuildChannel,
+    Integer
 };
 
 /**
  * A framework that simply doesn't сoсать anyone
- * @module Sosamba
  */
 module.exports = {
     Client,
@@ -67,7 +67,7 @@ module.exports = {
     MessageListener,
 
     /**
-     * Re-exported Eris library for instanceof checks :eyes:
+     * Re-exported Eris library for instanceof checks
      */
     Eris,
 };
