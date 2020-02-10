ef6136b365667d89f151acd94bdbe81659e6bf98
TTtie <tttiett@outlook.com>
2020-02-10 07:29:58 +0100

    Fix build

--- travis/build.sh
+++ travis/build.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-BOOL="$(git log -1 $TRAVIS_COMMIT --pretty="%aN")"
+BOOL="$(git log -1 $CURRENT_COMMIT --pretty="%aN")"
 if [ $BOOL == "tt-bot" ]; then
     echo "A commit is made by tt-bot, ignoring."
     exit 0
 else 
     echo "Building docs for $BOOL"
 fi
-
-echo "Checking out $TRAVIS_BRANCH..."
-git checkout $TRAVIS_BRANCH
 echo "Running ESLint..."
 npm test -- --fix
 
@@ -25,10 +22,10 @@ echo "Adding the documentation to git update list"
 git add . >/dev/null 2>&1
 
 echo "Committing..."
-git commit -m "Run ESlint and build documentation for $TRAVIS_COMMIT" >/dev/null 2>&1
+git commit -m "Run ESlint and build documentation for $CURRENT_COMMIT" >/dev/null 2>&1
 echo "Pushing.."
 
-git push https://tt-bot:$GIT_ACCESS_TOKEN@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH >/dev/null 2>&1
+git push https://$COMMIT_AUTHOR:$GIT_ACCESS_TOKEN@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH >/dev/null 2>&1
 
 echo "Successfully built and pushed the documentation"
 
--- travis/test.sh
+++ travis/test.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 set -e
-
-BOOL="$(git log -1 $TRAVIS_COMMIT --pretty="%aN")"
-if [ $BOOL == "tt-bot" ]; then
-    echo "A commit is made by tt-bot, ignoring."
-    exit 0
-else 
-    echo "Triggering the test for $BOOL"
-fi
 npm test
