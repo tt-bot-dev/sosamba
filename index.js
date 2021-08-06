a7b692beb52958e84105b3f033c220fa0989cdfe
TTtie <me@tttie.cz>
2021-08-06 13:55:25 +0000

    🔥 remove argument parsers
    Sosamba is moving to slash commands! As such, argument parsers won't be needed any more, since Discord will take care of it.
    However, this is a very, very raw removal, since there will be much more poking around all structures in Sosamba v2.

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const Client = require("./lib/Client");
 const Event = require("./lib/structures/Event");
 const Logger = require("./lib/util/Logger");
 const Command = require("./lib/structures/Command");
-const ArgumentParser = require("./lib/structures/ArgumentParser");
-const SimpleArgumentParser = require("./lib/argParsers/SimpleArgumentParser");
-const SwitchArgumentParser = require("./lib/argParsers/SwitchArgumentParser");
-const SerializedArgumentParser = require("./lib/argParsers/SerializedArgumentParser");
 const ParsingError = require("./lib/argParsers/ParsingError");
 const Constants = require("./lib/Constants");
 const ReactionMenu = require("./lib/structures/ReactionMenu");
@@ -34,10 +30,6 @@ module.exports = {
     Event,
     Logger,
     Command,
-    ArgumentParser,
-    SimpleArgumentParser,
-    SwitchArgumentParser,
-    SerializedArgumentParser,
     ParsingError,
     Constants,
     ReactionMenu,
