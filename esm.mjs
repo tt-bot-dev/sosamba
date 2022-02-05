51887b403e8743ea6fa3e13749f658f7703eb0e8
TTtie <me@tttie.cz>
2022-02-05 10:10:40 +0000

    ✨ add basic ESM support

---
+++ esm.mjs
@@ -0,0 +1,18 @@
+import Sosamba from "./index.js";
+
+export const {
+    Client,
+    Event,
+    Logger,
+    Command,
+    Constants,
+    ReactionMenu,
+    Serializers,
+    Structures,
+    MessageListener,
+    LocaleFormatter,
+    Locale,
+    InteractionContext,
+    InteractionListener,
+    Eris,
+} = Sosamba;
